86516522642d656ff723c5d03f02e5ba5c3205e5
Alex Smith <alex_smith3@hotmail.com>
2017-12-11 16:54:52 +0000

    update functions working

--- controllers/films.js
+++ controllers/films.js
@@ -6,28 +6,35 @@ var express = require('express');
 
 var filmRouter = new express.Router();
 
-
+// read
 filmRouter.get('/', function(req, res){
   res.json(films);
 });
+// works
 
+// create
 filmRouter.post('/', function(req, res){
   films.push(req.body.film);
   res.json({data: films});
 });
 
+// read
 filmRouter.get('/:id', function(req, res){
   res.json({data: films[req.params.id]})
 });
+// works
 
+// update
 filmRouter.put('/:id', function(req, res){
   films[req.params.id] = req.body.film;
   res.json({data: films});
 });
 
+// delete
 filmRouter.delete('/:id', function(req, res){
   films.splice(req.params.id, 1);
   res.json(films);
 });
+// works
 
 module.exports = filmRouter;
